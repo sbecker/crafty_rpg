3fe461d365ea3c32d0443d7d9d437a4a679dc23a
Scott Becker <scott.becker@jivesoftware.com>
2011-03-24 12:55:49 -0700

    Add movement controls

--- game.js
+++ game.js
@@ -73,8 +73,48 @@ window.onload = function() {
   Crafty.scene("main", function() {
     generateWorld();
 
+    Crafty.c('CustomControls', {
+      __move: {left: false, right: false, up: false, down: false},
+      _speed: 3,
+
+      CustomControls: function(speed) {
+        if (speed) this._speed = speed;
+        var move = this.__move;
+
+        this.bind('enterframe', function() {
+          // Move the player in a direction depending on the booleans
+          // Only move the player in one direction at a time (up/down/left/right)
+          if (move.right) this.x += this._speed;
+          else if (move.left) this.x -= this._speed;
+          else if (move.up) this.y -= this._speed;
+          else if (move.down) this.y += this._speed;
+        }).bind('keydown', function(e) {
+          // Default movement booleans to false
+          move.right = move.left = move.down = move.up = false;
+
+          // If keys are down, set the direction
+          if (e.keyCode === Crafty.keys.RA) move.right = true;
+          if (e.keyCode === Crafty.keys.LA) move.left = true;
+          if (e.keyCode === Crafty.keys.UA) move.up = true;
+          if (e.keyCode === Crafty.keys.DA) move.down = true;
+
+          this.preventTypeaheadFind(e);
+        }).bind('keyup', function(e) {
+          // If key is released, stop moving
+          if (e.keyCode === Crafty.keys.RA) move.right = false;
+          if (e.keyCode === Crafty.keys.LA) move.left = false;
+          if (e.keyCode === Crafty.keys.UA) move.up = false;
+          if (e.keyCode === Crafty.keys.DA) move.down = false;
+
+          this.preventTypeaheadFind(e);
+        });
+
+        return this;
+      }
+    });
+
     // Create our player entity with some premade components
-    var player = Crafty.e("2D, DOM, player, controls, animate, collision")
+    var player = Crafty.e("2D, DOM, player, controls, CustomControls, animate, collision")
       .attr({x: 160, y: 144, z: 1})
       .animate("walk_left", 6, 3, 8)
       .animate("walk_right", 9, 3, 11)
