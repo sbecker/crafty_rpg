6d560ea3ef78e2fc74c77b78a10888821975c7a5
Scott Becker <scott.becker@jivesoftware.com>
2011-03-24 12:06:45 -0700

    Add code for loading scene

--- game.js
+++ game.js
@@ -1,5 +1,23 @@
 window.onload = function() {
-  //start crafty
+  // Start crafty
   Crafty.init(50, 400, 320);
   Crafty.canvas();
+
+  // The loading screen that will display while our assets load
+  Crafty.scene("loading", function() {
+    // Load takes an array of assets and a callback when complete
+    Crafty.load(["sprite.png"], function() {
+      Crafty.scene("main"); //when everything is loaded, run the main scene
+    });
+
+    // Black background with some loading text
+    Crafty.background("#000");
+    Crafty.e("2D, DOM, text").attr({w: 100, h: 20, x: 150, y: 120})
+      .text("Loading")
+      .css({"text-align": "center"});
+  });
+
+  // Automatically play the loading scene
+  Crafty.scene("loading");
+
 };
